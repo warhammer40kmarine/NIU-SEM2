f37760d86ebeedda36107afed050b4b19464cb16
Daniel Chen <43944516+warhammer40kmarine@users.noreply.github.com>
2019-03-15 21:51:53 +0800

    REV.1.Initial concept

---
+++ Smart_Agriculture/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+void InitALL();
+void ReadTemp();
+int ReadHumidity();
+void OnOff();
+void Show();
+void Display();
+
+int main()
+{
+    cout << "Welcome to SMART AGRICULTURE test sensor Program" << endl;
+
+    return 0;
+}
+InitALL(int Num)
+{
+
+}
+ReadTemp(float Tmp) //溫度要能儲存小數點
+{
+    if(success)
+        return 0;
+    else
+        return -1;
+}
+ReadHumidity(float Hum) //ex. 60.5%
+{
+    if(success)
+        return 0;
+    else
+        return -1;
+}
+ReadCamera(char Pixel)//
+{
+   if(success)
+        return 0;
+    else
+        return -1;
+}
+OnOff(int I/O)
+{
+    if(success)
+        return 0;
+    else
+        return -1;
+}
+Show(int CLASS)
+{
+
+}
+Display(int wholedata)
+{
+
+}
