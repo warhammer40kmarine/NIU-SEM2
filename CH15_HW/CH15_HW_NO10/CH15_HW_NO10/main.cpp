b5c473a575f2d84b2b9e88ee76a0b681fb45cee2
Daniel Chen <43944516+warhammer40kmarine@users.noreply.github.com>
2019-05-01 00:45:56 +0800

    Finish

--- CH15_HW/CH15_HW_NO10/CH15_HW_NO10/main.cpp
+++ CH15_HW/CH15_HW_NO10/CH15_HW_NO10/main.cpp
@@ -24,21 +24,34 @@ class CTime
       {
          double time,s;
          int h,m;
-         time=this->to_sec()/var;
+         time=this->to_sec()*var;
          h=(int)time/3600;
          m=((int)time-h*3600)/60;
          s=time-(h*3600+m*60);
          return CTime(h,m,s);
       }
-};
 
+};
+CTime operator*(const int &var,CTime &t)
+        {
+            double time,s;
+            int h,m;
+            time=t.to_sec()*var;
+            h=(int)time/3600;
+            m=((int)time-h*3600)/60;
+            s=time-(h*3600+m*60);
+            return CTime(h,m,s);
+
+        }
 int main(void)
 {
    CTime t1(6,35,41.5);
    CTime t2=t1*3;
+   CTime t3=3*t1;
 
    t1.show_time();
    t2.show_time();
+   t3.show_time();
 
 
    return 0;
