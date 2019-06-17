23ac118545898ddb4a83ff3f5f601234c4ce7c7e
Daniel Chen <43944516+warhammer40kmarine@users.noreply.github.com>
2019-06-17 15:33:05 +0800

    6/17-old

--- test/main.cpp
+++ test/main.cpp
@@ -2,36 +2,13 @@
 #include <string>
 #include <cstdlib>
 using namespace std;
-union test
-    {
-        //char EngName;
-        int number;
-        int age;
-        int height;
-
-    };
-    union test sample; //={"anonymous",666,6,666};
-    //void func(struct sample);
+string f (string k);
 int main()
 {
-    //cout << "student's English Name=";
-    //getline(cin,sample.EngName);
-    cout << "student's tag number=";
-    cin>>sample.number;
-    cout << "student's age=";
-    cin>>sample.age;
-    cout << "student's height=";
-    cin>> sample.height;
+    if (string k<='z'&& string k>='a')
 
-
-    cout << "output of the test sample"<< endl;
-    cout << "number="<<sample.number << endl;
-    cout << "age="<<sample.age << endl;
-    cout << "height="<<sample.height<< endl;
-    //cout << "Name=" << sample.EngName<< endl;
-
-    cout << "sizeof(sample)="<< sizeof(sample);
-
+    string k;
+    cin << k;
 
 
     return 0;
